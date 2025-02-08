0a359c9abf64b65255dfa376744f901b8228e478
Bolang-Lonson <bbeinyoh12@gmail.com>
2025-02-08 08:24:20 +0100

    Refactor App.java to improve circle positioning and update launch method

--- gui-lesson/src/App.java
+++ gui-lesson/src/App.java
@@ -4,7 +4,7 @@ import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
-import javafx.scene.paint.Color;
+// import javafx.scene.paint.Color;
 import javafx.scene.layout.Pane;
 
 public class App extends Application {
@@ -13,6 +13,7 @@ public class App extends Application {
     public void start(Stage primaryStage) {
         StackPane pane = new StackPane();
         pane.getChildren().add(new Button("Stack Pane"));
+        
         Scene scene = new Scene(pane, 200, 50);
         primaryStage.setTitle("StackPane Demo");
         primaryStage.setScene(scene);
@@ -23,14 +24,13 @@ public class App extends Application {
         secondStage.setScene(new Scene(new Button("New Stage"), 500, 600));
         secondStage.show();
 
+        Pane pane1 = new Pane();
         Stage stage2 = new Stage();
         Circle circle= new Circle();
-        circle.setCenterX(100);
-        circle.setCenterY(100);
+        circle.centerXProperty().bind(pane1.widthProperty().divide(2));
+        circle.centerYProperty().bind(pane1.heightProperty().divide(2));
         circle.setRadius(50);
-        circle.setStroke(Color.LIGHTPINK);
-        circle.setFill(Color.BLUE);
-        Pane pane1 = new Pane();
+        circle.setStyle("-fx-stroke: black; -fx-fill: red;");
         pane1.getChildren().add(circle);
         Scene scene2 = new Scene(pane1, 200, 200);
         stage2.setTitle("Show Circle");
@@ -39,6 +39,6 @@ public class App extends Application {
         stage2.show();
     }
     public static void main(String[] args) throws Exception {
-        launch(); // Run this Application.
+        launch(args); // Run this Application.
     }
 }
